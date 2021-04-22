b88fbe3a34488189856e412b90adee60361b3fbb
Andy Hu <huandy01@gmail.com>
2021-04-22 21:11:34 +0200

    GUI erstellt

--- Game of Life Win Form/Game of Life Win Form/Menu_newGame.cs
+++ Game of Life Win Form/Game of Life Win Form/Menu_newGame.cs
@@ -12,6 +12,9 @@ namespace Game_of_Life_Win_Form
 {
     public partial class Menu_newGame : Form
     {
+        public int Y_Zahl { get; set; }
+        public int X_Zahl { get; set; }
+
         public Menu_newGame()
         {
             InitializeComponent();
@@ -19,7 +22,38 @@ namespace Game_of_Life_Win_Form
 
         private void btn_back_to_menu_main_Click(object sender, EventArgs e)
         {
-            //Menu_main.();
+            Menuing.OpenForm(new Menu_main(), this);
+        }
+
+        private void Y_Input_TextChanged(object sender, EventArgs e){}
+
+        private void X_Input_TextChanged(object sender, EventArgs e){}
+
+        private void btn_spielfeld_zufall_Click(object sender, EventArgs e)
+        {
+            Y_Zahl = new Random().Next(10, 51);
+            X_Zahl = new Random().Next(10, 51);
+        }
+
+        private void btn_eingaben_uebernehmen_Click(object sender, EventArgs e)
+        {
+            Y_Zahl = Int32.Parse(Y_Input.Text);
+            X_Zahl = Int32.Parse(X_Input.Text);
+        }
+
+        private void btn_zellen_zufall_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_spielfeld_aufrufen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_start_game_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
