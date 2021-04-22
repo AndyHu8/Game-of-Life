4b5b6b33cdeccc91010f23793a5f420120ffa72f
Andy Hu <huandy01@gmail.com>
2021-04-22 18:08:49 +0200

    zu WinForm gewechselt

---
+++ Game of Life Win Form/Game of Life Win Form/Menu_newGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_of_Life_Win_Form
+{
+    public partial class Menu_newGame : Form
+    {
+        public Menu_newGame()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_back_to_menu_main_Click(object sender, EventArgs e)
+        {
+            //Menu_main.();
+        }
+    }
+}
---
+++ Game of Life Win Form/Game of Life Win Form/Menuing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_of_Life_Win_Form
+{
+    public class Menuing : Form
+    {
+        public static Form CurrentForm { get; private set; }
+
+        public static void OpenForm(Form newForm, Form oldForm)
+        {
+            CurrentForm = CurrentForm != newForm ? newForm : CurrentForm;
+            CurrentForm.Show();
+            oldForm.Close();
+        }
+    }
+}
